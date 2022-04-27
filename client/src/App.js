34ec70cad3df7e9b1c33a56498b5a85b46abc327
RedSnoww12 <70609663+RedSnoww12@users.noreply.github.com>
2022-04-27 21:51:46 +0200

    first commit react

---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import {Article, Brand, CTA, Feature, Navbar} from './components';
+import {Footer, Blog, Features, Header, Possibility, WhatGPT3} from './containers';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
